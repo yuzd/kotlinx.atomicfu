2fe172dacfdbf9e13f462fb51a0c33d632de5ba2
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-10-04 13:14:45 +0300

    Workaround for new wasm target

--- atomicfu-gradle-plugin/src/main/kotlin/kotlinx/atomicfu/plugin/gradle/AtomicFUGradlePlugin.kt
+++ atomicfu-gradle-plugin/src/main/kotlin/kotlinx/atomicfu/plugin/gradle/AtomicFUGradlePlugin.kt
@@ -269,6 +269,7 @@ fun Project.configureMultiplatformPluginDependencies(version: String) {
                             KotlinPlatformType.jvm, KotlinPlatformType.androidJvm -> Platform.JVM
                             KotlinPlatformType.js -> Platform.JS
                             KotlinPlatformType.native -> Platform.NATIVE
+                            KotlinPlatformType.wasm -> Platform.JS
                         }
             val configurationName = when {
                 // impl dependency for native (there is no transformation)
